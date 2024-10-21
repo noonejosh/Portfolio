131edeec73ffc174ce9b63bb9dfaa5a43c2168ea
josh <138376982+noonejosh@users.noreply.github.com>
2024-10-21 16:56:08 +0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,14 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0";
+    } else {
+        sidebar.style.width = "250px";
+    }
+}
+
+function closeSidebar() {
+    document.getElementById("sidebar").style.width = "0";
+}
+
+
